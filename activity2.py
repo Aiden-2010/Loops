e2ff5b1a3dc48fd84c04e9d93a34cd3661768fe8
Aiden-2010 <aidenmartin.mgs@gmail.com>
2025-11-06 19:26:48 +0000

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,6 @@
+string = input("Please enter your string: ")
+string2 = (" ")
+for i in string:
+    string2 = i + string2
+print("\nThe orginal string =",string)
+print("\nThe reverse string =",string2)
